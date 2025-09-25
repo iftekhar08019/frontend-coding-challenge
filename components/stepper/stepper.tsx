ab5f5ad35a07998c9bdb34147157a5ff6daef5e8
Md Iftekharul Alam <68458610+iftekhar08019@users.noreply.github.com>
2025-09-25 12:14:45 +0200

    positioned correctly

--- components/stepper/stepper.tsx
+++ components/stepper/stepper.tsx
@@ -7,19 +7,19 @@ interface StepperProps {
 
 export default function Stepper({ steps, currentStep }: StepperProps) {
   return (
-    <div className="flex items-center justify-center ">
-      <ol className="flex items-center  w-[424px] lg:h-[132px] h-[80px] px-4 lg:py-8 py-4">
+    <div className="flex items-center justify-center w-full">
+      <ol className="flex items-center justify-center lg:w-[587px] w-[402px] lg:h-[132px] h-[80px]">
         {steps.map((step, index) => {
           const stepNumber = index + 1;
           const isCompleted = stepNumber <= currentStep; 
           const isActive = stepNumber === currentStep;
 
           return (
-            <li key={index} className="flex-1 flex items-center relative">
+            <li key={index} className="flex items-center justify-center relative flex-1">
               {/* Circle */}
               <div
-                className={`flex items-center justify-center lg:w-10 lg:h-10 w-[30px] h-[30px] rounded-full 
-                  ${isCompleted ? "bg-blue-600  text-white" : "bg-gray-200"}
+                className={`flex items-center justify-center lg:w-10 lg:h-10 w-[30px] h-[30px] rounded-full z-10
+                  ${isCompleted ? "bg-blue-600 text-white" : "bg-gray-200"}
                   ${isActive ? "border-blue-600 text-white" : ""}
                   ${
                     !isActive && !isCompleted
@@ -33,8 +33,8 @@ export default function Stepper({ steps, currentStep }: StepperProps) {
 
               {/* Label */}
               <span
-                className={`absolute top-10 left-1/2 -translate-x-1/2 text-xs 
-                  ${isActive || isCompleted ? "text-blue-600" : "text-gray-400"}
+                className={`absolute lg:top-14 top-10 left-1/2 transform -translate-x-1/2 text-base font-semibold whitespace-nowrap
+                  ${isActive || isCompleted ? "text-black" : "text-gray-400"}
                 `}
               >
                 {step.title}
@@ -43,7 +43,7 @@ export default function Stepper({ steps, currentStep }: StepperProps) {
               {/* Line between steps */}
               {index < steps.length - 1 && (
                 <div
-                  className={`flex-1 lg:h-[6px] h-[4px] 
+                  className={`absolute left-1/2 w-full lg:h-[6px] h-[4px] -z-10
                     ${isCompleted ? "bg-blue-600" : "bg-gray-200"}
                   `}
                 />
